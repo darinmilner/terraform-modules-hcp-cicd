9a61feaaa0dc8ccab7a1450281f79b0a727574b5
darinmilner <darin.milner@gmail.com>
2025-08-29 21:59:28 +0700

    Info pages module refactored

--- modules/info-pages/locals.tf
+++ modules/info-pages/locals.tf
@@ -1,6 +1,6 @@
 locals {
   repos = { for k, v in
-  data.data.terraform_remote_state.repos.outputs.clone_urls["prod"].clone_urls : k => v }
+  data.terraform_remote_state.repos.outputs.clone_urls["prod"].clone_urls : k => v }
   username = data.github_user.current_user.name
   avatar   = data.github_user.current_user.avatar_url
 }
